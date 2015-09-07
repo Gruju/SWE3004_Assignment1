26380a8c0b608adf8ddd02535c5b34f73ed01e14
Ben <wowmaniac2005@hotmail.com>
2015-09-08 03:06:05 +1000

    first commit

---
+++ swe3004/index.php
@@ -0,0 +1,114 @@
+<?php
+	echo "PHP APP";
+?>
+
+<!--<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>SWE3004 - Assignment 1</title>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	<meta name="description" content="SW30004: Software Deployment and Evolution - Assignment 1" />
+	<meta name="keywords" content="SWE3004" />
+</head>
+<body>
+<h1>SWE3004: Assignment 1</h1>
+<h2>Simple Web Application</h2>
+		<form id="standard" action="?action=submit" method="post" enctype="multipart/form-data">
+		<h4>How many days a person has been alive</h4>
+		<label for="name">Name: </label>
+		<input type="text" name="name" id="name"/>
+		<br/>
+		<br/>
+		<label for="dob-day">Day: </label>
+			<select class="round-border" id="dob-day" name="dob-day" data-validate="required:false">
+			<option value="">Please select...</option>
+			<option value="01">01</option>
+			<option value="02">02</option>
+			<option value="03">03</option>
+			<option value="04">04</option>
+			<option value="05">05</option>
+			<option value="06">06</option>
+			<option value="07">07</option>
+			<option value="08">08</option>
+			<option value="09">09</option>
+			<option value="10">10</option>
+			<option value="11">11</option>
+			<option value="12">12</option>
+			<option value="12">13</option>
+			<option value="12">14</option>
+			<option value="12">15</option>
+			<option value="12">16</option>
+			<option value="12">17</option>
+			<option value="12">18</option>
+			<option value="12">19</option>
+			<option value="12">20</option>
+			<option value="12">21</option>
+			<option value="12">22</option>
+			<option value="12">23</option>
+			<option value="12">24</option>
+			<option value="12">25</option>
+			<option value="12">26</option>
+			<option value="12">27</option>
+			<option value="12">28</option>
+			<option value="12">29</option>
+			<option value="12">30</option>
+			<option value="12">31</option>
+			</select>
+			
+		<label for="dob-month">Month: </label>
+			<select class="round-border" id="dob-month" name="dob-month" data-validate="required:false">
+			<option value="">Please select...</option>
+			<option value="01">01</option>
+			<option value="02">02</option>
+			<option value="03">03</option>
+			<option value="04">04</option>
+			<option value="05">05</option>
+			<option value="06">06</option>
+			<option value="07">07</option>
+			<option value="08">08</option>
+			<option value="09">09</option>
+			<option value="10">10</option>
+			<option value="11">11</option>
+			<option value="12">12</option>
+			</select>
+			
+		<label for="dob-year">Year:</label>
+			<select class="round-border" id="dob-year" name="dob-year" data-validate="required:false">
+			<option value="">Please select...</option>
+			<option value="1990">1990</option>
+			<option value="1991">1991</option>
+			<option value="1992">1992</option>
+			<option value="1993">1993</option>
+			<option value="1994">1994</option>
+			<option value="1995">1995</option>
+			<option value="1996">1996</option>
+			<option value="1997">1997</option>
+			<option value="1998">1998</option>
+			<option value="1999">1999</option>
+			<option value="2000">2000</option>
+			<option value="2001">2001</option>
+			<option value="2002">2002</option>
+			<option value="2003">2003</option>
+			<option value="2004">2004</option>
+			<option value="2005">2005</option>
+			<option value="2006">2006</option>
+			<option value="2007">2007</option>
+			<option value="2008">2008</option>
+			<option value="2009">2009</option>
+			<option value="2010">2010</option>
+			<option value="2011">2011</option>
+			<option value="2012">2012</option>
+			<option value="2013">2013</option>
+			<option value="2014">2014</option>
+			<option value="2015">2015</option>
+			</select>
+			
+			<br/>
+			<br/>
+		<div class="submit">
+			<input type="submit" id="submit_button" value="Calculate" />
+		</div>
+		<input type="hidden" name="FormSubmit" value="true" />
+		</form>
+</body>
+</html>-->
